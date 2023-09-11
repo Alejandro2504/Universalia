a19f58243ed04a5473c189b5ba9b4a01a0cf6f81
MARCHProyects <alejandroreyeshiguera@gmail.com>
2023-09-10 19:14:17 -0500

    AlphaBeta1

---
+++ Assets/UNIVERSALIA_GAME/SCRIPTS/Moneda.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moneda : MonoBehaviour
+{
+
+    [SerializeField] private float cantidadPuntos;
+
+    [SerializeField] private Puntaje puntaje;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        puntaje.SumarPuntos(cantidadPuntos);
+        //Destroy(gameObject);
+
+    }
+
+   }
+
